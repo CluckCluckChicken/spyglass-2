d9592bd3cf1233c9cbb0d43b2e8d59812fdb3b0f
potatophant <46281840+CluckCluckChicken@users.noreply.github.com>
2021-11-03 16:03:42 +0000

    ocular statuses

---
+++ src/lib/VerticalSeparator.svelte
@@ -0,0 +1,1 @@
+<h6 class="mx-3">&vert;</h6>
--- src/routes/users/[username].svelte
+++ src/routes/users/[username].svelte
@@ -10,6 +10,7 @@
     export let username;
 
     import CommentsView from "$lib/CommentsView.svelte";
+    import VerticalSeparator from "$lib/VerticalSeparator.svelte";
 
     async function getUser() {
         let response = await fetch(`${import.meta.env.VITE_API_HOST}/api/Scratch/users/${username}`);
@@ -18,6 +19,14 @@
     }
 
     let promise = getUser();
+
+    async function getOcularProfile() {
+        let response = await fetch(`https://my-ocular.jeffalo.net/api/user/${username}`);
+        
+        return await response.json();
+    }
+
+    let ocular = getOcularProfile();
 </script>
 
 {#await promise then result}
@@ -25,12 +34,27 @@
         <div class="lg:justify-center lg:items-center w-full mb-4">
             <div class="flex flex-col lg:justify-center lg:items-center lg:w-2/3 lg:mx-auto" style="max-width: 900px;">
                 <div class="flex flex-wrap my-4 w-full">
-                    <a href="/users/{username}" class="mr-2">
-                        <img src={user.profile.images.big} alt={user.username} class="rounded" style="max-width: 250px;"/>
+                    <a href="/users/{username}" class="mr-2 my-auto">
+                        <img src={user.profile.images.big} alt={user.username} class="rounded my-auto" style="max-width: 250px;"/>
                     </a>
-                    <h1>{user.username}</h1>
-                    <div class="flex-grow"></div>
-                    <a href="https://scratch.mit.edu/users/{user.username}" target="_blank" class="btn-default primary">View on Scratch</a>
+                    <div class="flex flex-col flex-grow">
+                        <div class="flex flex-wrap">
+                            <h1>{user.username}</h1>
+                            <div class="flex-grow"></div>
+                            <a href="https://scratch.mit.edu/users/{user.username}" target="_blank" class="btn-default primary">View on Scratch</a>    
+                        </div>
+                        <div class="flex flex-wrap -mt-2">
+                            <h6>{moment(user.history.joined).fromNow()}</h6>
+                        </div>
+                        <div class="flex flex-wrap">
+                            <h6>{user.profile.country}</h6>
+                            <VerticalSeparator/>
+                            {#await ocular then ocularProfile}
+                                <h6 class="italic mr-1">{ocularProfile.status}</h6>
+                                <span class="align-bottom ml-1 mt-3" style="width: 10px; height: 10px; background-color: {ocularProfile.color}; border-radius: 50%;"></span>
+                            {/await}
+                        </div>
+                    </div>
                 </div>
                 <div class="flex flex-row w-full">
                     <div class="flex flex-col max-w-sm ml-4 -mt-4">
