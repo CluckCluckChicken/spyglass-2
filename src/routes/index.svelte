8797601e714804894b7e416eb819853108e8be51
potatophant <46281840+CluckCluckChicken@users.noreply.github.com>
2021-12-09 20:02:13 +0000

    radical new update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,12 @@
-<h1>
+<script>
+    if (auth.isLoggedIn) {
+        window.location.href = "/browse";
+    }
+</script>
+
+{#if !auth.isLoggedIn}
+<h1 class="font-dosis">
     Magnifier
 </h1>
+<p class="text-3xl font-didactGothic">Magnifier is a comment reaction tool for <a href="https://scratch.mit.edu/">Scratch</a>. You can <a href="/auth">log in</a> to react to comments, and star them so that you can <a href="/starred">view them later</a>.</p>
+{/if}
--- src/routes/starred.svelte
+++ src/routes/starred.svelte
@@ -4,7 +4,11 @@ import CommentsView from "$lib/CommentsView.svelte";
 </script>
 <div class="ml-2">
     <h2>Starred Comments:</h2>
+    {#if auth.isLoggedIn}
     <h3>You can star comments by clicking ⭐ in the footer of the comment.</h3>
+    {:else}
+    <h3>You need to log in to star comments. After that, you can star comments by clicking ⭐ in the footer of the comment.</h3>
+    {/if}
 </div>
 
 <CommentsView url="{import.meta.env.VITE_API_HOST}/api/Stars" noPage/>
